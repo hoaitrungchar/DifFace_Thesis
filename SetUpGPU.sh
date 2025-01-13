bb2e79bb9bfdcbd256fc7a431708dceee5141ac3
hoaitrungchar <hoaitrungchar@gmail.com>
2025-01-13 03:15:06 +0000

    delete unnecessary file and upload config

--- SetUpGPU.sh
+++ SetUpGPU.sh
@@ -1,1 +1,2 @@
 export CUDA_VISIBLE_DEVICES="1,3,4,5,6"
+torchrun --standalone --nproc_per_node=1 --nnodes=1 main.py --cfg_path configs/training/predicted_edge_SwinUnet.yaml --save_dir /data/FFHQ/DifFace_prior
